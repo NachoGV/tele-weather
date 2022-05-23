ab30785943160d1d60f8cb270423f9f27f7389f8
aramoscupm <73371817+aramoscupm@users.noreply.github.com>
2022-05-23 21:56:35 +0200

    Added new connector
    It will be used for the second "Tesla.get" in forecast command

--- lib/tele_weather/tesla.ex
+++ lib/tele_weather/tesla.ex
@@ -6,6 +6,15 @@ defmodule TeslaApi do
     "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJpZ25hY2lvLmd2YWx2ZXJkZUBnbWFpbC5jb20iLCJqdGkiOiJmYWNiZWJiMS02ZTE1LTQ4Y2YtOTkwNy02ZTllN2NjOGFlZGEiLCJpc3MiOiJBRU1FVCIsImlhdCI6MTY1MTMzODgxMiwidXNlcklkIjoiZmFjYmViYjEtNmUxNS00OGNmLTk5MDctNmU5ZTdjYzhhZWRhIiwicm9sZSI6IiJ9.1J5Bg2Ybtup0cviQTfCT_etUY3A_iNZKjUH8y3oVmV8"
   end
 
+  def new_base(token \\ token_aemet()) do
+    middleware = [
+      {Tesla.Middleware.Timeout, timeout: 10000},
+      Tesla.Middleware.JSON,
+      {Tesla.Middleware.Headers, [{"api_key", "#{token}"}, {"Content-Type", "application/json"}]}
+    ]
+    Tesla.client(middleware, @adapter)
+  end
+
   def new(token \\ token_aemet()) do
     middleware = [
       {Tesla.Middleware.BaseUrl, "https://opendata.aemet.es/opendata/"},
@@ -18,6 +27,10 @@ defmodule TeslaApi do
 
   def get_muni(url) do
     Tesla.get(new(), url)
+  end
+
+  def get_muni_base(url) do
+    Tesla.get(new_base(), url)
   end
 
 end
