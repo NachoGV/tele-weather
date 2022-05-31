e1aeaa3299c5e00f27ef3a2789186b2eeadd6cc5
NachoGV <nachonio99@gmail.com>
2022-05-31 19:20:38 +0200

    Alerts for temperatura over X v1.0

--- lib/tele_weather/auxiliar.ex
+++ lib/tele_weather/auxiliar.ex
@@ -38,8 +38,67 @@ defmodule Aux do
     "#{Enum.at(String.split(Map.get(h, "fecha"), "T"), 0)}\nMáxima -> #{Map.get(Map.get(h, "temperatura"), "maxima")}ºC\nMínima -> #{Map.get(Map.get(h, "temperatura"), "minima")}ºC\n\n#{maps_to_string_week_forecast(t)}"
   end
 
-  def handle_alerts(code, chatid, op, temp) do
 
+  def handle_alerts(chatid, code, op, temp) do
+    {:ok, response} = TeslaApi.get_muni("api/prediccion/especifica/municipio/horaria/#{code}")
+    IO.inspect(response.body)
+    estado = Map.get(response.body, "estado")
+    case estado do
+      200 ->
+        url = Map.get(response.body,"datos")
+        {:ok, response} = TeslaApi.get_muni_base(url)
+        datos = Map.get(Map.get(List.first(Jason.decode!(Aux.latin1_to_utf8(response.body))), "prediccion"), "dia")
+        hora_actual = elem(elem(:calendar.local_time(), 1), 0)
+        check_temps(chatid, code, op, temp, datos, hora_actual, 6)
+      _other ->
+        ExGram.send_message(chatid, "Invalid code", token: "5132964358:AAGqPXBHHWQubRzXB-pOSKM7WAjjBlL4PDY")
+    end
+    Process.sleep(3600000)
+    handle_alerts(chatid, code, op, temp)
+  end
+
+  def check_temps(chatid, code, op, temp, [h|t], hora_actual, contador) do
+    if 23 - hora_actual < contador do
+      case op do
+        "<" ->
+          check_temp_under(chatid, temp, Map.get(h,"temperatura"), hora_actual, 23 - hora_actual, Enum.at(String.split(Map.get(h, "fecha"), "T"), 0))
+        ">" ->
+          check_temp_over(chatid, temp, Map.get(h,"temperatura"), hora_actual, 23 - hora_actual, Enum.at(String.split(Map.get(h, "fecha"), "T"), 0))
+      end
+      check_temps(chatid, code, op, temp, t, 0, contador-(23-hora_actual))
+    else
+      case op do
+        "<" ->
+          check_temp_under(chatid, temp, Map.get(h,"temperatura"), hora_actual, contador, Enum.at(String.split(Map.get(h, "fecha"), "T"), 0))
+        ">" ->
+          check_temp_over(chatid, temp, Map.get(h,"temperatura"), hora_actual, contador, Enum.at(String.split(Map.get(h, "fecha"), "T"), 0))
+      end
+    end
+  end
+
+  def check_temp_over(_chatid, _temp, _list, _hora_actual, 0, _fecha) do
+    nil
+  end
+
+  def check_temp_over(chatid, temp, [h|t], hora_actual, contador, fecha) do
+    hora = String.to_integer(Map.get(h, "periodo"))
+    value = Map.get(h, "value")
+    IO.inspect(contador)
+    IO.inspect(hora)
+    IO.inspect(hora_actual)
+    IO.puts("-----------")
+    if  hora > hora_actual do
+      if value > temp do
+        ExGram.send_message(chatid, "Date: #{fecha}\nTime: #{hora}h\nTemperature: #{value}ºC", token: "5132964358:AAGqPXBHHWQubRzXB-pOSKM7WAjjBlL4PDY")
+      end
+      check_temp_over(chatid, temp, t, hora_actual, contador-1, fecha)
+    else
+      check_temp_over(chatid, temp, t, hora_actual, contador, fecha)
+    end
+  end
+
+  def check_temp_under(chatid, temp, datos, cont, hora_actual, fecha) do
+    #ExGram.send_message(chatid, "Date: #{fecha}\nTime: #{}h\nTemperature: #{}ºC", token: "5132964358:AAGqPXBHHWQubRzXB-pOSKM7WAjjBlL4PDY")
   end
 
 end
--- lib/tele_weather/bot.ex
+++ lib/tele_weather/bot.ex
@@ -10,8 +10,9 @@ defmodule TeleWeather.Bot do
   command("getcodes", description: "List of region codes")
   command("forecast", description: "Get the daily forecast for the specified region for the next 48h")
   command("week_forecast", description: "Get temperatures for the next 6 days")
-  command("weather_alert", description: "Sets an weather alert for X < or > ºC in the next 48h")
-
+  command("weather_alert", description: "Sets a weather alert")
+  command("my_alerts", description: "List all the set alerts")
+  command("delete_alert", decription: "Delete the specified alert")
 
   def bot(), do: @bot
 
@@ -25,7 +26,9 @@ defmodule TeleWeather.Bot do
                     \n/getcodes\n File containing the list of region codes availible
                     \n/forecast [region_code]\n Get the daily forecast for the specified region\n Ex: /forecast 28115
                     \n/week_forecast [region_code]\n Get the week forecast for the specified region\n Ex: /week_forecast 28115
-                    \n/weather_alert [region_code] [<,>] [degrees]\n Sets an weather alert for checking if temperatures go over/down XºC in the next 48h\n Ex: /weather_alert 28115 > 30\n /weather_alert 28115 < 10")
+                    \n/weather_alert [region_code] [<,>] [degrees]\n Sets an weather alert for checking if temperatures go over/down XºC\n Ex: /weather_alert 28115 > 30\n /weather_alert 28115 < 10
+                    \n/my_alerts\n List all user's set alerts
+                    \n/delete_alert [alert_id]\n Deletes the specifies alert")
   end
 
   def handle({:command, :getcodes, _msg}, context) do
@@ -64,22 +67,26 @@ defmodule TeleWeather.Bot do
     code = Enum.at(String.split(alert.text, " "), 0)
     op = Enum.at(String.split(alert.text, " "), 1)
     temp = Enum.at(String.split(alert.text, " "), 2)
-    {:ok, response} = TeslaApi.get_muni("api/prediccion/especifica/municipio/diaria/#{code}")
-    case Map.get(response.body, "estado") do
-      200 ->
-        datos = List.first(Jason.decode!(Aux.latin1_to_utf8(response.body)))
-        pid = spawn(fn -> Aux.handle_alerts(context.update.message.chat.id, code, op, temp) end)
+    chatid = context.update.message.chat.id
+    pid = spawn(fn -> Aux.handle_alerts(chatid, code, op, temp) end)
 
-        IO.inspect(pid)
-        # añadir a ETS alertas => {pid}
+    IO.inspect(pid)
+    # añadir a ETS alertas => {pid, chatid, :weather_alert, op, temp}
 
-        case op do
-          "<" -> answer(context, "Alarm set for temperatures under #{temp} in #{Map.get(datos, "nombre")}")
-          ">" -> answer(context, "Alarm set for temperatures over #{temp} in #{Map.get(datos, "nombre")}")
-        end
-      _other ->
-        answer(context, "Invalid code")
+    case op do
+      "<" -> answer(context, "Alarm set for temperatures under #{temp}")
+      ">" -> answer(context, "Alarm set for temperatures over #{temp}")
     end
+
+  end
+
+
+  def handle({:command, :my_alerts, _msg}, context) do
+    answer(context, "To be implemented ;)")
+  end
+
+  def handle({:command, :delete_alert, _alert_id}, context) do
+    answer(context, "To be implemented ;)")
   end
 
 end
